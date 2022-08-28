da64c7c2d1cb802556a7e8d76d0e940f49ff588d
Brian Ibrahim Qamardeen <brian@qeetell.eon>
2022-08-28 23:09:31 +0100

    Ds progress

--- va00Sbjc.c
+++ va00Sbjc.c
@@ -2,10 +2,22 @@
 #include "va50Blck.h"
 
 int main () {
-        struct  qbqetell_etnica_BLCK* name;
-        int _ba00 = 1000;
-        int _bb00 = -2;
-        _bb00 = qbqetell_etnica_BLCK____intl (name, &_ba00);
-        printf ("Outcome is: %d\n", _bb00);
+        qbqetell_etnica_BLCK* infr;
+        int _ba00_ = -900; int* _ba00 = &_ba00_;
+        int _bb00_ = -100; int* _bb00 = &_bb00_;
+        (*_bb00) = qbqetell_etnica_BLCK____intl (&infr, _ba00 + 0);
+        if ((*_bb00) == 0) {
+                printf ("Init errr: %d\n", (*_bb00));
+                return 1;
+        }
+        printf ("Test stts: 10--00\n");
+        (*((*infr).strm +    0)) = 11;
+        printf ("Test stts: 20--00\n");
+        (*((*infr).strm + 1023)) = 22;
+        printf ("Test stts: 30--00\n");
+        printf ("Init data: %d\n", (*((*infr).strm +    0)));
+        printf ("Test stts: 40--00\n");
+        printf ("Init data: %d\n", (*((*infr).strm + 1023)));
+        printf ("Test stts: PASSED\n");
         return 0;
 }
--- va50Blck.h
+++ va50Blck.h
@@ -1,52 +1,70 @@
 #include <stdlib.h>
 
-struct qbqetell_etnica_BLCK {
+struct qbqetell_etnica_BLCK_ {
         char* strm;
         int*  vlmm;
         int*  cvrg;
         struct qbqetell_etnica_BLCK* extnBlck;
 };
-        int qbqetell_etnica_BLCK____intl (struct qbqetell_etnica_BLCK* sbjc, int* vlmm) {
-                if (sbjc == NULL) {
-                        sbjc = (struct qbqetell_etnica_BLCK*) calloc (1,
-                                sizeof (struct qbqetell_etnica_BLCK));
+typedef struct qbqetell_etnica_BLCK_ qbqetell_etnica_BLCK;
+        
+        int qbqetell_etnica_BLCK____intl (qbqetell_etnica_BLCK** sbjc, int* vlmm) {
+                if ((*sbjc) == NULL) {
+                        (*sbjc) = (qbqetell_etnica_BLCK*) calloc (1,
+                                sizeof (qbqetell_etnica_BLCK));
                 }
                 if (vlmm == NULL || (*vlmm) < 1 || (*vlmm) > 1024) return 0;
                 
-                if ((*sbjc).strm != NULL) {
-                        free ((*sbjc).strm);
-                        (*sbjc).strm = NULL;
+                if ((*(*sbjc)).strm != NULL) {
+                        free ((*(*sbjc)).strm);
+                        (*(*sbjc)).strm = NULL;
                 }
-                if ((*sbjc).vlmm != NULL) {
-                        free ((*sbjc).vlmm);
-                        (*sbjc).vlmm = NULL;
+                if ((*(*sbjc)).strm != NULL) {
+                        free ((*(*sbjc)).strm);
+                        (*(*sbjc)).vlmm = NULL;
                 }
-                if ((*sbjc).cvrg != NULL) {
-                        free ((*sbjc).cvrg);
-                        (*sbjc).cvrg = NULL;
+                if ((*(*sbjc)).strm != NULL) {
+                        free ((*(*sbjc)).strm);
+                        (*(*sbjc)).cvrg = NULL;
                 }
                 
-                (*sbjc).strm    = (char*) calloc ((*vlmm), sizeof (char));
-                (*sbjc).vlmm    = (int* ) calloc (1, sizeof (int));
-                (*(*sbjc).vlmm) = (*vlmm);
-                (*sbjc).cvrg    = (int* ) calloc (1, sizeof (int));
-                (*(*sbjc).cvrg) = 0;
+                (*(*sbjc)).strm = (char*) calloc ((*vlmm), sizeof (char));
+                (*(*sbjc)).vlmm    = (int* ) calloc (1,       sizeof (int ));
+                (*(*(*sbjc)).vlmm) = (*vlmm);
+                (*(*sbjc)).cvrg    = (int* ) calloc (1,       sizeof (int ));
+                (*(*(*sbjc)).cvrg) = 0;
+                
+                if ((*(*sbjc)).strm == NULL || (*(*sbjc)).vlmm == NULL || (*(*sbjc)).cvrg == NULL) {
+                        if ((*(*sbjc)).strm != NULL) {
+                                free ((*(*sbjc)).strm);
+                                (*(*sbjc)).strm = NULL;
+                        }
+                        if ((*(*sbjc)).vlmm != NULL) {
+                                free ((*(*sbjc)).strm);
+                                (*(*sbjc)).vlmm = NULL;
+                        }
+                        if ((*(*sbjc)).cvrg != NULL) {
+                                free ((*(*sbjc)).strm);
+                                (*(*sbjc)).cvrg = NULL;
+                        }
+                        return 0;
+                }
                 
                 return 1;
         }
-        int qbqetell_etnica_BLCK____dntl (struct qbqetell_etnica_BLCK* sbjc) {
-                if ((*sbjc).strm     != NULL) {
-                        free ((*sbjc).strm);
-                        (*sbjc).strm = NULL;
+        int qbqetell_etnica_BLCK____dntl (qbqetell_etnica_BLCK** sbjc) {
+                if ((*(*sbjc)).strm     != NULL) {
+                        free ((*(*sbjc)).strm);
+                        (*(*sbjc)).strm = NULL;
                 }
-                if ((*sbjc).vlmm     != NULL) {
-                        free ((*sbjc).vlmm);
-                        (*sbjc).vlmm = NULL;
+                if ((*(*sbjc)).vlmm     != NULL) {
+                        free ((*(*sbjc)).vlmm);
+                        (*(*sbjc)).vlmm = NULL;
                 }
-                if ((*sbjc).cvrg     != NULL) {
-                        free ((*sbjc).cvrg);
-                        (*sbjc).cvrg = NULL;
+                if ((*(*sbjc)).cvrg     != NULL) {
+                        free ((*(*sbjc)).cvrg);
+                        (*(*sbjc)).cvrg = NULL;
                 }
-                if ((*sbjc).extnBlck != NULL) (*sbjc).extnBlck = NULL;
+                if ((*(*sbjc)).extnBlck != NULL) (*(*sbjc)).extnBlck = NULL;
                 return 1;
         }
