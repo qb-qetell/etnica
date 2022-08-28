9bd58a68d9c94d73db4bd7141ced7bae668532a0
Brian Ibrahim Qamardeen <brian@qeetell.eon>
2022-08-28 22:11:02 +0100

    Ds progress

---
+++ va00Sbjc.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "va50Blck.h"
+
+int main () {
+        struct  qbqetell_etnica_BLCK* name;
+        int _ba00 = 1000;
+        int _bb00 = -2;
+        _bb00 = qbqetell_etnica_BLCK____intl (name, &_ba00);
+        printf ("Outcome is: %d\n", _bb00);
+        return 0;
+}
